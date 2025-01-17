8cdff5676e9a069d5ca85e0c20f7ce5a3acbb252
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-17 22:31:09 +0200

    Add Update and Add Image Request

---
+++ src/main/java/com/example/demo/request/AddImageRequest.java
@@ -0,0 +1,21 @@
+package com.example.demo.request;
+
+import com.example.demo.model.entity.Product;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.sql.Blob;
+
+@Data
+@AllArgsConstructor
+public class AddImageRequest {
+    private String url;
+    @Lob
+    private Blob image;
+    private String title;
+    private Product product;
+
+}
+
+
