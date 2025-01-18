a0c3f1be1afb479c33a5ed31e16102da1a593ad4
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-18 22:59:31 +0200

    Add ImageDto

--- src/main/java/com/example/demo/model/dto/ImageDto.java
+++ src/main/java/com/example/demo/model/dto/ImageDto.java
@@ -1,4 +1,21 @@
 package com.example.demo.model.dto;
 
-public class ImageDTO {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Blob;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageDto {
+
+    private Long id;
+    private String url;
+    private Blob image;
+    private String title;
+    private ProductDto productDTO;
 }
