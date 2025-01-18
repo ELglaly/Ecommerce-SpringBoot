8b39a1895d45960262ee0f6d671d0de15f3678cb
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-18 23:01:53 +0200

    Add ProductMapper Class

--- src/main/java/com/example/demo/model/mapping/ProductMapper.java
+++ src/main/java/com/example/demo/model/mapping/ProductMapper.java
@@ -1,4 +1,40 @@
 package com.example.demo.model.mapping;
 
+import com.example.demo.model.dto.ProductDto;
+import com.example.demo.model.entity.Product;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Builder
+@Data
 public class ProductMapper {
+
+    public static ProductDto toDto(Product product) {
+        
+        return ProductDto.builder()
+                .id(product.getId())
+                .brand(product.getBrand())
+                .description(product.getDescription())
+                .name(product.getName())
+                .price(product.getPrice())
+                .quantity(product.getQuantity())
+                .categoryDto(CategoryMapper.toDto(product.getCategory()))
+                .build();
+    }
+
+
+    public static Product toEntity(ProductDto productDto) {
+
+        return Product.builder()
+                .brand(productDto.getBrand())
+                .description(productDto.getDescription())
+                .id(productDto.getId())
+                .name(productDto.getName())
+                .price(productDto.getPrice())
+                .quantity(productDto.getQuantity())
+                .category(CategoryMapper.toEntity(productDto.getCategoryDto()))
+                .images(productDto.getImageDto())
+                .build();
+    }
 }
