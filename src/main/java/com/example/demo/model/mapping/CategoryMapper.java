7cff7cac643348e616a3891e3e8e21af33b038eb
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-18 23:00:25 +0200

    Add CategoryMapper Class

--- src/main/java/com/example/demo/model/mapping/CategoryMapper.java
+++ src/main/java/com/example/demo/model/mapping/CategoryMapper.java
@@ -1,4 +1,34 @@
 package com.example.demo.model.mapping;
 
+import com.example.demo.model.dto.CategoryDto;
+import com.example.demo.model.dto.ProductDto;
+import com.example.demo.model.entity.Category;
+
+import java.util.stream.Collectors;
+
 public class CategoryMapper {
+
+    public static CategoryDto toDto(Category category) {
+        return CategoryDto.builder()
+                .id(category.getId())
+                .description(category.getDescription())
+                .name(category.getName())
+                .productsDto(category.getProducts().stream()
+                        .map(ProductMapper::toDto)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    public static Category toEntity(CategoryDto categoryDto) {
+
+        return Category.builder()
+                .id(categoryDto.getId())
+                .description(categoryDto.getDescription())
+                .name(categoryDto.getName())
+                .products(categoryDto.getProductsDto().stream()
+                        .map(ProductMapper::toEntity)
+                        .collect(Collectors.toList())
+                )
+                .build();
+    }
 }
