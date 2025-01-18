fa85ccecb24874161afd164ead431b8889482a39
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-18 23:00:47 +0200

    Add ImageMapper Class

--- src/main/java/com/example/demo/model/mapping/ImageMapper.java
+++ src/main/java/com/example/demo/model/mapping/ImageMapper.java
@@ -1,4 +1,27 @@
 package com.example.demo.model.mapping;
 
+import com.example.demo.model.dto.ImageDto;
+import com.example.demo.model.entity.Image;
+
 public class ImageMapper {
+
+    public static Image toEntity(ImageDto imageDto) {
+        return Image.builder()
+                .url(imageDto.getUrl())
+                .id(imageDto.getId())
+                .title(imageDto.getTitle())
+                .product(ProductMapper.toEntity(imageDto.getProductDTO()))
+                .image(imageDto.getImage())
+                .build();
+    }
+
+    public static ImageDto toDto(Image image) {
+        return ImageDto.builder()
+                .url(image.getUrl())
+                .id(image.getId())
+                .title(image.getTitle())
+                .productDTO(ProductMapper.toDto(image.getProduct()))
+                .image(image.getImage())
+                .build();
+    }
 }
