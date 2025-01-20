850bfbe6b40fed9fdd6303802fccf09c479d6402
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 00:24:27 +0200

    Updated ResourceAlreadyExistsException.java file

--- src/main/java/com/example/demo/exceptions/ResourceAlreadyExistsException.java
+++ src/main/java/com/example/demo/exceptions/ResourceAlreadyExistsException.java
@@ -1,8 +1,16 @@
 package com.example.demo.exceptions;
 
-public class ResourceAlreadyExistsException extends RuntimeException{
-    public ResourceAlreadyExistsException(String message) {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Locale;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ResourceAlreadyExistsException extends AppException{
+    public ResourceAlreadyExistsException(String message, String resourceName) {
         super(message);
+        super.setErrorCode(resourceName.toUpperCase()+ "_ALREADY_EXISTS");
+        super.setStatus(HttpStatus.CONFLICT);
     }
 
 }
