909e0a2e2e59e1e86d8a6a2a9a6fc93bca79fc89
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 00:24:06 +0200

    Add InvalidFieldException.java file

--- src/main/java/com/example/demo/exceptions/InvalidFieldException.java
+++ src/main/java/com/example/demo/exceptions/InvalidFieldException.java
@@ -1,4 +1,12 @@
 package com.example.demo.exceptions;
 
-public class InvalidFieldException {
+import org.springframework.http.HttpStatus;
+
+public class InvalidFieldException extends AppException {
+
+    public InvalidFieldException(String message) {
+        super("Invalid field: "+message);
+        super.setErrorCode("INVALID_FIELD");
+        super.setStatus(HttpStatus.BAD_REQUEST);
+    }
 }
