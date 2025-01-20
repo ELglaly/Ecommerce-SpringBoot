10c539a097c1a2f9a9bfc490b271bb942bc56169
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 00:25:35 +0200

    Add ValidationException.java file

--- src/main/java/com/example/demo/exceptions/ValidationException.java
+++ src/main/java/com/example/demo/exceptions/ValidationException.java
@@ -1,4 +1,13 @@
 package com.example.demo.exceptions;
 
-public class ValidationException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ValidationException extends AppException {
+    public ValidationException(String message) {
+        super("Validation error: "+message);
+        setErrorCode("VALIDATION_ERROR");
+        setStatus(HttpStatus.BAD_REQUEST);
+    }
 }
