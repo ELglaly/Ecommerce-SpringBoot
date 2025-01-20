56ba4ec29fb2e3ca9d4375b374be6f9827428edb
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 00:25:46 +0200

    Updated CategoryDto.java file

--- src/main/java/com/example/demo/model/dto/CategoryDto.java
+++ src/main/java/com/example/demo/model/dto/CategoryDto.java
@@ -1,25 +1,98 @@
 package com.example.demo.model.dto;
 
-
-import com.example.demo.model.entity.Category;
-import com.example.demo.model.entity.Product;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+
+
+
 public class CategoryDto {
 
     private Long id;
+    @NotNull(message = "Name cannot be null")
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    @Pattern(regexp = "^[^\\d]*$", message = "Name must not contain digits")
     private String name;
+
+    @NotNull(message = "Description cannot be null")
+    @NotBlank(message = "Description cannot be Empty")
+    @Size(min = 5, max = 500, message = "Description must be between 5 and 500 characters")
     private String description;
+    @Valid
     private List<ProductDto> productsDto;
 
 
+    public CategoryDto(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.description = builder.description;
+        this.productsDto = builder.productsDto;
+    }
+    public CategoryDto() { }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String description;
+        private List<ProductDto> productsDto;
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+        public Builder productsDto(List<ProductDto> productsDto) {
+            this.productsDto = productsDto;
+            return this;
+        }
+        public CategoryDto build() {
+            return new CategoryDto(this);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<ProductDto> getProductsDto() {
+        return productsDto;
+    }
+
+    public void setProductsDto(List<ProductDto> productsDto) {
+        this.productsDto = productsDto;
+    }
 }
