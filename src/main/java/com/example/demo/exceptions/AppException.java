de9555694f9f16af46d863ba9c644f6888133af7
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 00:22:36 +0200

    Updated AppException.java file

--- src/main/java/com/example/demo/exceptions/AppException.java
+++ src/main/java/com/example/demo/exceptions/AppException.java
@@ -1,4 +1,23 @@
 package com.example.demo.exceptions;
 
-public class AppException {
+import org.springframework.http.HttpStatus;
+
+public class AppException extends RuntimeException {
+    private String errorCode;
+    private HttpStatus status;
+    public AppException(String message) {
+        super(message);
+    }
+    public String getErrorCode() {
+        return errorCode;
+    }
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+    public HttpStatus getStatus() {
+        return status;
+    }
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
 }
