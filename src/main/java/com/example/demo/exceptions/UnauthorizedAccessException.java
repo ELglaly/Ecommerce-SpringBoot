12b31423b8d56ecf28fe651b818d396ed7eeaa50
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 00:25:16 +0200

    Add UnauthorizedAccessException.java file

--- src/main/java/com/example/demo/exceptions/UnauthorizedAccessException.java
+++ src/main/java/com/example/demo/exceptions/UnauthorizedAccessException.java
@@ -1,4 +1,13 @@
 package com.example.demo.exceptions;
 
-public class UnauthorizedAccessException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizedAccessException extends AppException {
+    public UnauthorizedAccessException(String message) {
+        super("Unauthorized access : "+message);
+        super.setStatus(HttpStatus.UNAUTHORIZED);
+        super.setErrorCode("UNAUTHORIZED_ACCESS");
+    }
 }
