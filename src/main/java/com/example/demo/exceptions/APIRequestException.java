e6079f04e7dcb5cee781735604107e61e30768f5
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 00:22:26 +0200

    Updated APIRequestException.java file

--- src/main/java/com/example/demo/exceptions/APIRequestException.java
+++ src/main/java/com/example/demo/exceptions/APIRequestException.java
@@ -1,4 +1,13 @@
 package com.example.demo.exceptions;
 
-public class APIRequestException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class APIRequestException extends AppException {
+    public APIRequestException(String endPoint) {
+        super("API request error: "+endPoint);
+        super.setStatus(HttpStatus.BAD_REQUEST);
+        super.setErrorCode("API_REQUEST_ERROR");
+    }
 }
