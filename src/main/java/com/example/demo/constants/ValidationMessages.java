69382c98c31dcf7351b8a87d3a01ad353452e89b
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 03:02:09 +0200

    Add ValidationMessages.java for validation-related message management

--- src/main/java/com/example/demo/constants/ValidationMessages.java
+++ src/main/java/com/example/demo/constants/ValidationMessages.java
@@ -1,4 +1,8 @@
 package com.example.demo.constants;
 
 public class ValidationMessages {
+    public static final String REQUIRED_FIELD = "This field is required.";
+    public static final String INVALID_EMAIL = "Please provide a valid email address.";
+    public static final String PASSWORD_TOO_SHORT = "Password must be at least 8 characters.";
+    public static final String INVALID_DATE = "Invalid date format.";
 }
