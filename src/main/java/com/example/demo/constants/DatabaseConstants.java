d845cf8a643e067a99f263492d65e9b285523b62
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 03:01:41 +0200

    Add DatabaseConstants.java to centralize database-related constants

--- src/main/java/com/example/demo/constants/DatabaseConstants.java
+++ src/main/java/com/example/demo/constants/DatabaseConstants.java
@@ -1,4 +1,7 @@
 package com.example.demo.constants;
 
+
 public class DatabaseConstants {
+    public static final String DEFAULT_SCHEMA = "public";
+    public static final int MAX_BATCH_SIZE = 1000;
 }
