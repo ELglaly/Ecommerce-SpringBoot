92797bf226e4ee33ef4f0bd5f97aec245e660271
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 03:01:59 +0200

    Add PaginationConstants.java for pagination-related constants

--- src/main/java/com/example/demo/constants/PaginationConstants.java
+++ src/main/java/com/example/demo/constants/PaginationConstants.java
@@ -1,4 +1,7 @@
 package com.example.demo.constants;
 
 public class PaginationConstants {
+    public static final int DEFAULT_PAGE_SIZE = 20;
+    public static final int DEFAULT_PAGE_NUMBER = 0;
+    public static final int MAX_PAGE_SIZE = 100;
 }
