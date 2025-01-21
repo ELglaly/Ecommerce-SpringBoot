f80234e81d5b198581dd316f5ce3d6e845999071
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-21 03:01:06 +0200

    Modified ApiConstants.java for enhanced API constant definitions

--- src/main/java/com/example/demo/constants/ApiConstants.java
+++ src/main/java/com/example/demo/constants/ApiConstants.java
@@ -1,4 +1,8 @@
 package com.example.demo.constants;
 
 public class ApiConstants {
+    public static final String BASE_API = "/api/v1";
+    public static final String CATEGORY_ENDPOINT = BASE_API + "/categories";
+    public static final String PRODUCT_ENDPOINT = BASE_API + "/products";
+    public static final String USER_ENDPOINT = BASE_API + "/users";
 }
