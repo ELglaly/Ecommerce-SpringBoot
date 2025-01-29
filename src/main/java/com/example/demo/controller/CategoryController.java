8748ee403f887e2d3db58c89027ef03adaddbec3
Sherif Elglaly <sherifelglaly@gmail.com>
2025-01-29 18:04:38 +0200

    Modified CategoryController.java

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-
+import com.example.demo.constants.*;
 import com.example.demo.model.dto.CategoryDto;
 import com.example.demo.response.ApiResponse;
 import com.example.demo.serivce.category.CategoryService;
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("${api.prefix}/categories")
+@RequestMapping(ApiConstants.CATEGORY_ENDPOINT)
 public class CategoryController {
 
     private final CategoryService categoryService;
@@ -19,7 +21,6 @@ public class CategoryController {
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
-
 
     @PostMapping("/add")
     public ResponseEntity<ApiResponse> addCategory(@RequestBody @Valid CategoryDto categoryDto) {
@@ -29,9 +30,27 @@ public class CategoryController {
     }
 
     @GetMapping ("/get/{name}")
-    public ResponseEntity<CategoryDto> getCategoryByName(@PathVariable String name) {
+    public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name) {
         CategoryDto categoryDto = categoryService.getCategoryByName(name);
-        return ResponseEntity.ok(categoryDto);
+        return ResponseEntity.ok(new ApiResponse(categoryDto,"Category found!"));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<ApiResponse> getAllCategory() {
+
+        List<CategoryDto> categoryDtos = categoryService.getAllCategories();
+        return ResponseEntity.ok(new ApiResponse(categoryDtos,"Category found!") );
+    }
+
+    @GetMapping("/getCount")
+    public ResponseEntity<ApiResponse> getCategoryCount() {
+        return ResponseEntity.ok(new ApiResponse(categoryService.getCategoryCount(),"Category found!"));
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.ok(new ApiResponse(true,"Category deleted!"));
     }
 
     @PutMapping ("/update/{id}")
