07b4ea4dee1ed65cc12aa5787726a7a1aba36368
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 03:40:17 +0200

    A IddCartItemService interface with new methods

--- src/main/java/com/example/demo/serivce/cart/ICartItemService.java
+++ src/main/java/com/example/demo/serivce/cart/ICartItemService.java
@@ -1,4 +1,13 @@
 package com.example.demo.serivce.cart;
 
-public class ICartItemService {
+import com.example.demo.model.entity.CartItem;
+
+import java.util.List;
+
+public interface ICartItemService {
+
+        void addItemToCart(Long cartId, Long productId, int quantity);
+        void deleteItemFromCart(Long cartId, Long productId);
+        void updateItemQuantity(Long cartId, Long productId, int quantity);
+        CartItem getCartItem(Long cartId, Long productId);
 }
