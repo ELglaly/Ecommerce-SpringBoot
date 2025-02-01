52ddbcd43f8095820433bfd120feb5aecac2f55a
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 03:42:22 +0200

    Update ICartService interface with new cart-related methods

--- src/main/java/com/example/demo/serivce/cart/ICartService.java
+++ src/main/java/com/example/demo/serivce/cart/ICartService.java
@@ -1,4 +1,13 @@
 package com.example.demo.serivce.cart;
 
+import com.example.demo.model.entity.Cart;
+
+import java.math.BigDecimal;
+
 public interface ICartService {
+
+    Cart getCart(Long id);
+    void clearCart(Long id);
+    BigDecimal getTotalPrice(Long id);
+
 }
