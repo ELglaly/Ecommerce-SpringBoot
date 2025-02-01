d5c477ec7315a222a781aa7ba39f1deadcf5945c
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 22:43:46 +0200

    Update Order requests in the request package

--- src/main/java/com/example/demo/request/order/AddOrderRequest.java
+++ src/main/java/com/example/demo/request/order/AddOrderRequest.java
@@ -1,4 +1,21 @@
-package com.example.demo.request;
+package com.example.demo.request.order;
+
+import com.example.demo.enums.OrderStatus;
+import com.example.demo.model.dto.UserDto;
+import com.example.demo.model.entity.OrderItem;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AddOrderRequest {
+
+    private Long orderId;
+    private LocalDate orderDate;
+    private BigDecimal orderTotalPrice;
+    private OrderStatus orderStatus;
+    private Set<OrderItem> orderItems=new HashSet<>();
+    private UserDto user;
+
 }
--- src/main/java/com/example/demo/request/order/UpdateOrderItemRequest.java
+++ src/main/java/com/example/demo/request/order/UpdateOrderItemRequest.java
@@ -1,4 +1,16 @@
 package com.example.demo.request.order;
 
+import com.example.demo.model.dto.ProductDto;
+import com.example.demo.model.dto.order.OrderDto;
+
+import java.math.BigDecimal;
+
 public class UpdateOrderItemRequest {
+
+    private int quantity;
+    private BigDecimal unitePrice;
+    private BigDecimal totalPrice;
+    private OrderDto order;
+
+    private ProductDto product;
 }
