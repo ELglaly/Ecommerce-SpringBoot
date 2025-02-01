c2b4c898e1822b75f98fa269d1d678378318bcbe
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 03:40:45 +0200

    Add IOrderService interface with new order methods

--- src/main/java/com/example/demo/serivce/order/IOrderService.java
+++ src/main/java/com/example/demo/serivce/order/IOrderService.java
@@ -1,4 +1,17 @@
 package com.example.demo.serivce.order;
 
+import com.example.demo.enums.OrderStatus;
+import com.example.demo.model.dto.order.OrderDto;
+
+import java.util.List;
+
 public interface IOrderService {
+
+    OrderDto placeOrder(Long userId);
+    OrderDto getOrderById(Long orderId);
+    OrderDto updateOrder(Long orderId, OrderDto orderDto);
+    List<OrderDto> getAllOrders();
+    List<OrderDto> getOrdersByUserId(Long userId);
+    List<OrderDto> getOrdersByUserIdAndStatus(Long userId, OrderStatus status);
+
 }
