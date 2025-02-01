211dd528e49153ba6b2a1872e45e9dd165977f09
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 22:41:57 +0200

    Update Repository package

--- src/main/java/com/example/demo/repository/OrderRepository.java
+++ src/main/java/com/example/demo/repository/OrderRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo.repository;
 
+import com.example.demo.enums.OrderStatus;
+import com.example.demo.model.dto.order.OrderDto;
 import com.example.demo.model.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
+
+    List<Order> findByOrderStatusAndUserId(OrderStatus status, Long userId);
 }
