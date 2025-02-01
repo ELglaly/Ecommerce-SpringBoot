278afc8fe04e69958c59e594bb2f08a3f9018b5b
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 03:40:30 +0200

    Add IOrderItem interface with new order item methods

--- src/main/java/com/example/demo/serivce/order/IOrderItem.java
+++ src/main/java/com/example/demo/serivce/order/IOrderItem.java
@@ -1,4 +1,9 @@
 package com.example.demo.serivce.order;
 
+import com.example.demo.model.dto.order.OrderDto;
+
 public interface IOrderItem {
+
+    OrderDto getOrder();
+    void setOrder(AddOrderRequest request);
 }
