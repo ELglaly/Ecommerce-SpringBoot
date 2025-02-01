a0f3e8a6261028bc67e059f8e27d0a660d486a3c
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 03:38:42 +0200

    Add UserRepository with new user query methods

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,4 +1,18 @@
 package com.example.demo.repository;
+import com.example.demo.model.entity.User;
+import jakarta.validation.constraints.Email;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+
+    User findByUsername(String username);
+
+    User findByEmailAndPassword(String email, String password);
+    User findByUsernameAndPassword(String username, String password);
+
+    boolean existsByUsername(String username);
+
+
+    boolean existsByEmail(@Email(message = "Must be a valid Email") String email);
 }
