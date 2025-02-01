abb2a39c66463b812dae680610ace714f3a51faf
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 03:37:47 +0200

    A add rderItem entity with new order item details

--- src/main/java/com/example/demo/model/entity/OrderItem.java
+++ src/main/java/com/example/demo/model/entity/OrderItem.java
@@ -1,4 +1,36 @@
 package com.example.demo.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    private BigDecimal unitePrice;
+    private BigDecimal totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public OrderItem(int quantity, Product product, Order order, BigDecimal unitePrice) {
+        this.quantity = quantity;
+        this.product = product;
+        this.order = order;
+        this.unitePrice = unitePrice;
+    }
 }
