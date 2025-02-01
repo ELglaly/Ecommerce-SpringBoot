ba6fb45d2dc1d548da294050f33e3e2049285b3d
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 03:42:26 +0200

    Update IUserService interface with new user-related methods

--- src/main/java/com/example/demo/serivce/user/IUserService.java
+++ src/main/java/com/example/demo/serivce/user/IUserService.java
@@ -1,4 +1,15 @@
 package com.example.demo.serivce.user;
 
+import com.example.demo.model.dto.UserDto;
+import com.example.demo.request.CreateUserRequest;
+import com.example.demo.request.UpdateUserRequest;
+
 public interface IUserService {
+    UserDto getUserById(Long id);
+    UserDto getUserByEmail(String email);
+    UserDto getUserByUsername(String username);
+    UserDto createUser(CreateUserRequest request);
+    UserDto updateUser(UpdateUserRequest request);
+    void deleteUser(Long id);
+
 }
