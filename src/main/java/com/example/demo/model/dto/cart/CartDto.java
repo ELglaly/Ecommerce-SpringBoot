1565a37a60f5dbeb88c43d77b630a3d81ede6a83
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 03:34:53 +0200

    Update CartDto to include new cart properties

--- src/main/java/com/example/demo/model/dto/cart/CartDto.java
+++ src/main/java/com/example/demo/model/dto/cart/CartDto.java
@@ -1,4 +1,57 @@
-package com.example.demo.model.dto;
+package com.example.demo.model.dto.cart;
+
+import com.example.demo.model.entity.CartItem;
+import com.example.demo.model.entity.User;
+
+import java.math.BigDecimal;
+import java.util.Set;
 
 public class CartDto {
+
+    private Long id;
+    private BigDecimal totalPrice;
+    private int totalAmount=0;
+    private Set<CartItem> items;
+    private User user;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(int totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Set<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<CartItem> items) {
+        this.items = items;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
