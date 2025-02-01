ba277cacb73aab65e2fb46715d8e279fa6aa0085
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 03:36:47 +0200

    add  Address entity with new fields

--- src/main/java/com/example/demo/model/entity/Address.java
+++ src/main/java/com/example/demo/model/entity/Address.java
@@ -1,4 +1,86 @@
 package com.example.demo.model.entity;
 
+import com.example.demo.exceptions.ValidationException;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        if(zip.matches("\\d{5}"))
+        {
+            this.zip = zip;
+        }
+        else
+            throw new ValidationException("Invalid zip code");
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
