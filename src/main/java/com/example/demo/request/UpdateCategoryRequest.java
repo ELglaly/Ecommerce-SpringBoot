df896c825bdaefc1eeb6ef9bf4b537df92c80899
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-01 03:39:23 +0200

    Update UpdateCategoryRequest with new category update logic

--- src/main/java/com/example/demo/request/UpdateCategoryRequest.java
+++ src/main/java/com/example/demo/request/UpdateCategoryRequest.java
@@ -8,9 +8,9 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 import java.util.List;
-    @Data
     @AllArgsConstructor
     public class UpdateCategoryRequest {
 
@@ -27,4 +27,29 @@ import java.util.List;
         @Valid
         private List<ProductDto> productsDto;
 
-}
+
+        public @NotNull(message = "Name cannot be null") @NotBlank(message = "Name cannot be empty") @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters") @Pattern(regexp = "^[^\\d]*$", message = "Name must not contain digits") String getName() {
+            return name;
+        }
+
+        public void setName(@NotNull(message = "Name cannot be null") @NotBlank(message = "Name cannot be empty") @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters") @Pattern(regexp = "^[^\\d]*$", message = "Name must not contain digits") String name) {
+            this.name = name;
+        }
+
+        public @Valid List<ProductDto> getProductsDto() {
+            return productsDto;
+        }
+
+        public void setProductsDto(@Valid List<ProductDto> productsDto) {
+            this.productsDto = productsDto;
+        }
+
+        public @NotNull(message = "Description cannot be null") @NotBlank(message = "Description cannot be Empty") @Size(min = 5, max = 500, message = "Description must be between 5 and 500 characters") String getDescription() {
+            return description;
+        }
+
+        public void setDescription(@NotNull(message = "Description cannot be null") @NotBlank(message = "Description cannot be Empty") @Size(min = 5, max = 500, message = "Description must be between 5 and 500 characters") String description) {
+            this.description = description;
+        }
+
+    }
