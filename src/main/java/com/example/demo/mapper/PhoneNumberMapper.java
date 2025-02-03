4cb6672636b15e6b1cb742c321a956c6b7154d8c
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-03 06:07:55 +0200

    Add PhoneNumberMapper: Implements mapping for PhoneNumber to PhoneNumberDto

--- src/main/java/com/example/demo/mapper/PhoneNumberMapper.java
+++ src/main/java/com/example/demo/mapper/PhoneNumberMapper.java
@@ -1,4 +1,15 @@
 package com.example.demo.mapper;
 
-public class PhoneNumberMapper {
+import com.example.demo.model.dto.PhoneNumberDto;
+import com.example.demo.model.entity.PhoneNumber;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class PhoneNumberMapper implements IPhoneNumberMapper {
+    @Override
+    public Set<PhoneNumberDto> toDto(Set<PhoneNumber> phoneNumber) {
+        return null;
+    }
 }
