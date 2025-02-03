c4ecdad38f5a548d950fab3b08f8414dcca1ca07
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-03 06:08:03 +0200

    Add IUserMapper: Interface for mapping User to UserDto

--- src/main/java/com/example/demo/mapper/IUserMapper.java
+++ src/main/java/com/example/demo/mapper/IUserMapper.java
@@ -1,4 +1,8 @@
 package com.example.demo.mapper;
 
+import com.example.demo.model.dto.UserDto;
+import com.example.demo.model.entity.User;
+
 public interface IUserMapper {
+    UserDto toDto(User user);
 }
