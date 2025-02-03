8b376fef7b077c1a29e6ffd557b30fa0c49ad59b
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-03 06:12:10 +0200

    modify ProductMapper

--- src/main/java/com/example/demo/mapper/ProductMapper.java
+++ src/main/java/com/example/demo/mapper/ProductMapper.java
@@ -1,4 +1,43 @@
 package com.example.demo.mapper;
 
-public class ProductMapper {
+import com.example.demo.model.dto.ImageDto;
+import com.example.demo.model.dto.ProductDto;
+import com.example.demo.model.entity.Product;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.request.product.AddProductRequest;
+import com.example.demo.request.product.UpdateProductRequest;
+import org.apache.catalina.mapper.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ProductMapper implements IProductMapper {
+    private final ModelMapper modelMapper;
+    public ProductMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public Product toEntity(AddProductRequest request) {
+        return modelMapper.map(request, Product.class);
+    }
+
+    public Product toEntity(ProductDto request) {
+        return modelMapper.map(request, Product.class);
+    }
+
+    public ProductDto toDto(Product product) {
+        ProductDto productDto = modelMapper.map(product, ProductDto.class);
+        List<ImageDto> imageDto = product.getImages().stream()
+                .map(image -> modelMapper.map(image, ImageDto.class))
+                .collect(Collectors.toList());
+        productDto.setImageDto(imageDto);
+        return productDto;
+    }
+
+    public Product toEntity(UpdateProductRequest request) {
+        return null;
+    }
 }
