aad6014a8974a4dc2eae0ffb609fb06be492d077
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-03 06:11:48 +0200

    modify ProductMapper

--- src/main/java/com/example/demo/mapper/IProductMapper.java
+++ src/main/java/com/example/demo/mapper/IProductMapper.java
@@ -1,4 +1,15 @@
 package com.example.demo.mapper;
 
+import com.example.demo.model.dto.ProductDto;
+import com.example.demo.model.entity.Product;
+import com.example.demo.request.product.AddProductRequest;
+import com.example.demo.request.product.UpdateProductRequest;
+
 public interface IProductMapper {
+
+    Product toEntity(AddProductRequest product);
+    ProductDto toDto(Product product);
+    Product toEntity(UpdateProductRequest product);
+    Product toEntity(ProductDto product);
+
 }
