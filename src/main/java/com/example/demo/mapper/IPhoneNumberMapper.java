b78161a73be67f1cd219c17dbe3c6eb11aa0bafa
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-03 06:07:49 +0200

    Add IPhoneNumberMapper: Interface for mapping PhoneNumber to PhoneNumberDto

--- src/main/java/com/example/demo/mapper/IPhoneNumberMapper.java
+++ src/main/java/com/example/demo/mapper/IPhoneNumberMapper.java
@@ -1,4 +1,11 @@
 package com.example.demo.mapper;
 
+import com.example.demo.model.dto.PhoneNumberDto;
+import com.example.demo.model.entity.PhoneNumber;
+
+import java.util.Set;
+
 public interface IPhoneNumberMapper {
+
+    Set<PhoneNumberDto> toDto(Set<PhoneNumber> phoneNumber);
 }
