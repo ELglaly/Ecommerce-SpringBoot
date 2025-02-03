7c897fcb05dbfdc5c5dc62d6871ff6e1f384bee6
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-03 06:08:08 +0200

    Add UserMapper: Implements mapping for User to UserDto

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,4 +1,32 @@
 package com.example.demo.mapper;
 
-public class UserMapper {
+import com.example.demo.model.dto.UserDto;
+import com.example.demo.model.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper implements IUserMapper
+{
+    private final IPhoneNumberMapper phoneNumberMapper;
+    private final IAddressMapper addressMapper;
+
+    public UserMapper(IPhoneNumberMapper phoneNumberMapper,IAddressMapper addressMapper)
+    {
+        this.phoneNumberMapper = phoneNumberMapper;
+        this.addressMapper = addressMapper;
+    }
+
+    @Override
+    public UserDto toDto(User user) {
+        return new UserDto.Builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .birthDate(user.getBirthDate())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .address(addressMapper.toDto(user.getAddress()))
+                .phoneNumbers(phoneNumberMapper.toDto(user.getPhoneNumber()))
+                .build();
+    }
 }
