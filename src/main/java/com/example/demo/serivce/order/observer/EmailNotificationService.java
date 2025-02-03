7d71a2eff00bdc8cb3ec4d02e8aa9d8d9e445aa1
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-03 06:08:30 +0200

    Add EmailNotificationService: Sends email notifications for order events

--- src/main/java/com/example/demo/serivce/order/observer/EmailNotificationService.java
+++ src/main/java/com/example/demo/serivce/order/observer/EmailNotificationService.java
@@ -1,4 +1,12 @@
 package com.example.demo.serivce.order.observer;
 
-public class EmailNotificationService {
+import com.example.demo.model.entity.Order;
+
+import java.util.Observer;
+
+public class EmailNotificationService implements OrderObserver {
+    @Override
+    public void update(Order order) {
+        System.out.println(order.getOrderId() + " " + order.getOrderStatus());
+    }
 }
