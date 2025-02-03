5456d86c2d77399924886f5b890e4479be2cf8f7
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-03 06:07:38 +0200

    Add IAddressMapper: Interface for mapping Address to AddressDto

--- src/main/java/com/example/demo/mapper/IAddressMapper.java
+++ src/main/java/com/example/demo/mapper/IAddressMapper.java
@@ -1,4 +1,9 @@
 package com.example.demo.mapper;
 
+import com.example.demo.model.dto.AddressDto;
+import com.example.demo.model.entity.Address;
+
 public interface IAddressMapper {
+
+    AddressDto toDto(Address address);
 }
