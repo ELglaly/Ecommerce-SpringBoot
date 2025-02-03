7fb57d43d62cc03698215106bcee211389b60b9d
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-03 06:08:13 +0200

    Add OrderItemMapper: Maps OrderItem entity to OrderItemDto

--- src/main/java/com/example/demo/mapper/order/OrderItemMapper.java
+++ src/main/java/com/example/demo/mapper/order/OrderItemMapper.java
@@ -1,4 +1,26 @@
 package com.example.demo.mapper.order;
 
-public class OrderItemMapper {
+import com.example.demo.mapper.IProductMapper;
+import com.example.demo.mapper.OrderMapper;
+import com.example.demo.model.dto.order.OrderItemDto;
+import com.example.demo.model.entity.OrderItem;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderItemMapper implements IOrderItemMapper {
+    private IProductMapper productMapper;
+
+    public OrderItemMapper(IProductMapper productMapper) {
+        this.productMapper = productMapper;
+    }
+    @Override
+    public OrderItemDto toDto(OrderItem orderItem) {
+        return new OrderItemDto.Builder()
+                .id(orderItem.getId())
+                .quantity(orderItem.getQuantity())
+                .totalPrice(orderItem.getTotalPrice())
+                .unitePrice(orderItem.getUnitePrice())
+                .productDto(productMapper.toDto(orderItem.getProduct()))
+                .build();
+    }
 }
