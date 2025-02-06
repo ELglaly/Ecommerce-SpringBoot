1a57060175b2c55bd3577b5a64ef1f762ca3fc37
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-06 07:36:17 +0200

    Add lazylaoding to image file

--- src/main/java/com/example/demo/model/entity/Image.java
+++ src/main/java/com/example/demo/model/entity/Image.java
@@ -1,71 +1,54 @@
 package com.example.demo.model.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.sql.Blob;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 
 @Entity
 @Table(name="Images")
-@NoArgsConstructor
+@NoArgsConstructor 
 @AllArgsConstructor
 @Setter
 @Getter
+@Builder
+@ToString(exclude = {"image", "product"})
+@EqualsAndHashCode(exclude = {"image", "product"})
 public class Image {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(length = 512)
     private String url;
 
     @Lob
+    @Basic(fetch = FetchType.LAZY)
     private Blob image;
-    private String name;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn()
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
     private Product product;
-
-
-    public Image(Builder builder) {
-        this.id = builder.id;
-        this.url = builder.url;
-        this.image = builder.image;
-        this.name = builder.name;
-        this.product = builder.product;
-    }
-
-    public static class Builder {
-        private Long id;
-        private String url;
-        private Blob image;
-        private String name;
-        private Product product;
-
-        public Builder id(Long id) {
-            this.id = id;
-            return this;
-        }
-        public Builder url(String url) {
-            this.url = url;
-            return this;
-        }
-        public Builder image(Blob image) {
-            this.image = image;
-            return this;
-        }
-        public Builder name(String name) {
-            this.name = name;
-            return this;
-        }
-        public Builder product(Product product) {
-            this.product = product;
-            return this;
-        }
-        public Image build() {
-            return new Image(this);
-        }
-    }
 
 }
