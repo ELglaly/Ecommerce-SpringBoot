85acdeea344ee109ecf64a67646d7c029310b1e4
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-16 03:44:42 +0200

    = Implement user registration and login functionality.

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,4 +1,32 @@
 package com.example.demo.controller;
 
+import com.example.demo.constants.ApiConstants;
+import com.example.demo.model.dto.UserDto;
+import com.example.demo.request.user.LoginRequest;
+import com.example.demo.response.ApiResponse;
+import com.example.demo.serivce.user.IUserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ApiConstants.LOGIN)
 public class LoginController {
+    private final IUserService userService;
+    public LoginController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    // TODO: Implement login endpoint
+    // implement login endpoint
+   @GetMapping()
+    public ResponseEntity<ApiResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
+        UserDto userDto = userService.login(loginRequest.getUsernameOrEmail(), loginRequest.getPassword());
+        return ResponseEntity.ok(new ApiResponse(userDto, "Login successful"));
+    }
+    // TODO: Implement logout endpoint
+    // TODO: Implement login with Google endpoint
 }
--- src/main/java/com/example/demo/controller/RegisterController.java
+++ src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,4 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.constants.ApiConstants;
+import com.example.demo.model.dto.UserDto;
+import com.example.demo.request.user.CreateUserRequest;
+import com.example.demo.response.ApiResponse;
+import com.example.demo.serivce.user.IUserManagementService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(ApiConstants.REGISTER)
 public class RegisterController {
+    private final IUserManagementService userManagementService;
+    public RegisterController(IUserManagementService userManagementService) {
+        this.userManagementService = userManagementService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<ApiResponse> register(@RequestBody @Valid CreateUserRequest createUserRequest) {
+        UserDto userDto = userManagementService.createUser(createUserRequest);
+        return ResponseEntity.ok(new ApiResponse(userDto, "User created successfully"));
+    }
+    // TODO: Implement register with Google endpoint
+
 }
