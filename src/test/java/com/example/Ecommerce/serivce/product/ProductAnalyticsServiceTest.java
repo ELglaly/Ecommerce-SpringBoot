60b69beba1c44e902973637958de6e7b4911f489
Sherif Elglaly <sherifelglaly@gmail.com>
2025-02-28 00:00:58 +0200

    test(product): add unit tests for product service

---
+++ src/test/java/com/example/Ecommerce/serivce/product/ProductAnalyticsServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.Ecommerce.serivce.product;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductAnalyticsServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getProductsCountByCategory() {
+    }
+
+    @Test
+    void getProductsCountByBrand() {
+    }
+
+    @Test
+    void getProductsCountByName() {
+    }
+}
