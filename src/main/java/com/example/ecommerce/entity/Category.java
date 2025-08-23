57db481156caef6172d75143a92efe89eeadd3e1
Sherif Elglaly <sherifelglaly@gmail.com>
2025-08-23 21:16:07 +0300

    feat(category): enhance category entity with audit fields
    Add audit fields and improve validation.
    
    - Introduce created_at, updated_at, created_by, and updated_by fields for auditing.
    - Refactor validation messages to CategoryUtils.
    - Update regex for category name validation.

--- src/main/java/com/example/ecommerce/entity/Category.java
+++ src/main/java/com/example/ecommerce/entity/Category.java
@@ -1,13 +1,18 @@
 package com.example.ecommerce.entity;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.ecommerce.util.CategoryUtils;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Getter
 @Setter
@@ -22,16 +27,29 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Name is required")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "name must contain only letters")
+    @NotBlank(message = CategoryUtils.INVALID_CATEGORY_NAME)
+    @Pattern(regexp = CategoryUtils.CATEGORY_NAME_PATTERN,message = CategoryUtils.CATEGORY_NAME_PATTERN_MESSAGE)
     @Column(nullable = false)
     private String name;
 
-    @Size(min = 3, max = 500, message = "Description must be between 3 and 50 characters")
-    @Pattern(regexp = "^[a-zA-Z]+$", message = "Description must contain only letters")
-    @NotBlank(message = "Description is required")
+    @Size(min = 3, max = 500, message = CategoryUtils.CATEGORY_DESCRIPTION_SIZE)
+    @NotBlank(message = CategoryUtils.INVALID_CATEGORY_NAME)
     @Column(length = 500)
     private String description;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_by", length = 100)
+    private String createdBy;
+
+    @Column(name = "updated_by", length = 100)
+    private String updatedBy;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL,
             orphanRemoval = true, fetch = FetchType.LAZY)
