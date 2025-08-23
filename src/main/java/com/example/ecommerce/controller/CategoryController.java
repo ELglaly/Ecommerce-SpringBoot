7285118d89b5eda533e620e4b220d222f30950c7
Sherif Elglaly <sherifelglaly@gmail.com>
2025-08-23 21:15:22 +0300

    feat(category): enhance category management
    Implement search, update messages, and pagination.
    
    - Implement category search by name.
    - Update success messages to use constants.
    - Add pagination to get all categories.

--- src/main/java/com/example/ecommerce/controller/CategoryController.java
+++ src/main/java/com/example/ecommerce/controller/CategoryController.java
@@ -5,21 +5,24 @@ import com.example.ecommerce.dto.CategoryDTO;
 import com.example.ecommerce.request.category.AddCategoryRequest;
 import com.example.ecommerce.request.category.UpdateCategoryRequest;
 import com.example.ecommerce.response.ApiResponse;
-import com.example.ecommerce.serivce.category.ICategoryService;
+import com.example.ecommerce.serivce.category.CategoryService;
+import com.example.ecommerce.util.CategoryUtils;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
 @RequestMapping(ApiConstants.CATEGORY_ENDPOINT)
 public class CategoryController {
 
-    private final ICategoryService categoryService;
+    private final CategoryService categoryService;
 
-    public CategoryController(ICategoryService categoryService) {
+    public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
@@ -27,39 +30,39 @@ public class CategoryController {
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse> addCategory(@RequestBody @Valid AddCategoryRequest request) {
         CategoryDTO category = categoryService.addCategory(request);
-        return ResponseEntity.ok(new ApiResponse(category,"Added Successfully!"));
+        return ResponseEntity.ok(new ApiResponse(category, CategoryUtils.CATEGORY_ADDED));
     }
 
     @GetMapping ("/search/")
-    public ResponseEntity<ApiResponse> getCategoryByName(@RequestParam String name) {
-        CategoryDTO categoryDto = categoryService.getCategoryByName(name);
-        return ResponseEntity.ok(new ApiResponse(categoryDto,"Category found!"));
+    public ResponseEntity<ApiResponse> searchByName(@RequestParam String name) {
+        Page<CategoryDTO> categoryDTOS = categoryService.searchByName(name);
+        return ResponseEntity.ok(new ApiResponse(categoryDTOS,CategoryUtils.CATEGORY_FETCHED));
     }
 
     @GetMapping()
     public ResponseEntity<ApiResponse> getAllCategory() {
 
-        List<CategoryDTO> categoryDTOS = categoryService.getAllCategories();
-        return ResponseEntity.ok(new ApiResponse(categoryDTOS,"Category found!") );
+        Page<CategoryDTO> categoryDTOS = categoryService.getAllCategories();
+        return ResponseEntity.ok(new ApiResponse(categoryDTOS, CategoryUtils.CATEGORY_FETCHED) );
     }
 
     @GetMapping("/count")
     public ResponseEntity<ApiResponse> getCategoryCount() {
-        return ResponseEntity.ok(new ApiResponse(categoryService.getCategoryCount(),"Category found!"));
+        return ResponseEntity.ok(new ApiResponse(categoryService.getCategoryCount(),CategoryUtils.CATEGORY_FETCHED));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id) {
         categoryService.deleteCategory(id);
-        return ResponseEntity.ok(new ApiResponse(true,"Category deleted!"));
+        return ResponseEntity.ok(new ApiResponse(true,CategoryUtils.CATEGORY_DELETED));
     }
 
     @PutMapping ("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody @Valid UpdateCategoryRequest request) {
             CategoryDTO categoryDto = categoryService.updateCategory(request,id);
-            return ResponseEntity.ok(new ApiResponse(categoryDto,"Category Updated!"));
+            return ResponseEntity.ok(new ApiResponse(categoryDto,CategoryUtils.CATEGORY_UPDATED));
 
     }
 
